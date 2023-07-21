8757ad710617686fabb3eb522661d902a1929f01
Carlos3108 <c.silva3108@hotmail.com>
2023-07-21 00:00:46 -0300

    mudanca no export de alunos

--- fundamentos-react/src/data/alunos.js
+++ fundamentos-react/src/data/alunos.js
@@ -1,11 +1,8 @@
-export default [
+const alunos = [
     {id: 1, nome: 'Carlos', nota: 9.0},
     {id: 2, nome: 'Thais', nota: 7.0},
     {id: 3, nome: 'Vitoria', nota: 10.0},
     {id: 4, nome: 'Sueli', nota: 6.0},
-    {id: 5, nome: 'Flávio', nota: 8.0},
-    {id: 6, nome: 'Josinaldo', nota: 7.6},
-    {id: 7, nome: 'Branco', nota: 8.5},
-    {id: 8, nome: 'Cida', nota: 7.4},
-    {id: 9, nome: 'Vera', nota: 6.7},
+    {id: 5, nome: 'Flávio', nota: 8.0}
 ]
+export default alunos;
